60c61c6be4c5dc9534ec385e6d853b5e9672cb5f
Dubois Guillaume <guillaume.dubois@epitech.eu>
2024-11-03 10:29:37 -0500

    Updates

---
+++ _app/immutable/chunks/paths.DlSeJCdu.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1e24vq6)==null?void 0:s.base)??"/Dubois-Guillaume.github.io";var e;const t=((e=globalThis.__sveltekit_1e24vq6)==null?void 0:e.assets)??a;export{t as a,a as b};
